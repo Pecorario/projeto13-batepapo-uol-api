1495527e409156ba27950619de9e3e7d8993e0e5
Taynara Pecorario <taynara_rp22@hotmail.com>
2023-04-12 18:19:27 -0300

    :sparkles: adding participants get and post

--- src/app.js
+++ src/app.js
@@ -1,30 +1,61 @@
+import { MongoClient } from 'mongodb';
+
 import express from 'express';
 import cors from 'cors';
-import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
-dotenv.config();
+import joi from 'joi';
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br.js';
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
-// const db = mongoClient.db();
 let db;
 mongoClient
   .connect()
   .then(() => (db = mongoClient.db()))
-  .catch(err => console.log(err.message));
+  .catch(error => console.log(error.message));
+
+const participantSchema = joi.object({
+  name: joi.string().required()
+});
+
+const messageSchema = joi.object({
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().required()
+});
 
 app.post('/participants', async (req, res) => {
   try {
-    const { name } = req.body;
+    const validation = participantSchema.validate(req.body, {
+      abortEarly: true
+    });
+    const participants = await db.collection('participants').find().toArray();
 
-    if (!name || name.trim() === '') {
+    if (validation.error) {
       return res.sendStatus(422);
     }
 
-    await db.collection('participants').insertOne({ name });
+    if (participants.find(participant => participant.name === req.body.name)) {
+      return res.sendStatus(409);
+    }
+
+    await db
+      .collection('participants')
+      .insertOne({ name: req.body.name, lastStatus: Date.now() });
+
+    await db.collection('messages').insertOne({
+      from: req.body.name,
+      to: 'Todos',
+      text: 'entra na sala...',
+      type: 'status',
+      time: dayjs().format('HH:mm:ss')
+    });
     return res.sendStatus(201);
   } catch (error) {
     return res.status(500).send(error.message);
@@ -34,6 +65,16 @@ app.post('/participants', async (req, res) => {
 app.get('/participants', async (req, res) => {
   try {
     const data = await db.collection('participants').find().toArray();
+
+    return res.send(data);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+app.get('/messages', async (req, res) => {
+  try {
+    const data = await db.collection('messages').find().toArray();
 
     return res.send(data);
   } catch (error) {
